f03518a1644da7e1c15fe2a2538a39294043fb6a
Sanika Sajwani <162726771+SanikaSajwani@users.noreply.github.com>
2025-03-27 21:39:15 +0500

    Create Lab4-Task2.java

---
+++ Lab4-Task2.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main (String[] args) {
+    String[] names = { "Sam", "Sara", "Ali", "San", "Shaaz"};
+
+    for (String name: names) {
+      System.out.println(name);
+    }
+  }
+}
