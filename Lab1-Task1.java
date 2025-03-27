c8994cf539de6f2a2edf5d98b32d8ddf33f0cbe1
Sanika Sajwani <162726771+SanikaSajwani@users.noreply.github.com>
2025-03-27 17:28:47 +0500

    Create Lab1-Task1.java

---
+++ Lab1-Task1.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main (String[] args){
+    System.out.println("Introduction to JAVA");
+    System.out.print("Lab 1 of Java Object Oriented Programming\n");
+    System.out.printf("FAST NUCES");
+  }
+}
