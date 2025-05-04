7b1767d2dc4036640e73e6b0f14588dc10add45c
Sanika Sajwani <162726771+SanikaSajwani@users.noreply.github.com>
2025-05-04 22:37:50 +0500

    Create Lab10-Task1.java

---
+++ Lab10-Task1.java
@@ -0,0 +1,25 @@
+abstract class Parent {
+    abstract void message();
+}
+
+class FirstSubclass extends Parent {
+    void message() {
+        System.out.println("This is first subclass");
+    }
+}
+
+class SecondSubclass extends Parent {
+    void message() {
+        System.out.println("This is second subclass");
+    }
+}
+
+public class Task1 {
+    public static void main(String[] args) {
+        Parent obj1 = new FirstSubclass();
+        Parent obj2 = new SecondSubclass();
+
+        obj1.message();
+        obj2.message();
+    }
+}
